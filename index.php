1e0d418c0cb6ca6710234df6b3385700311b5a21
Mathieu <brunotmathieu340@gmail.com>
2025-03-14 14:39:56 +0100

    liaison js et csssur index.php

--- index.php
+++ index.php
@@ -5,12 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>TAUPE 10 JEUX ROBLOX</title>
-    <link rel="stylesheet" href="site.css">
+    <link rel="stylesheet" href="css/site.css">
 </head>
 <body>
     
-<h1>OOF</h1>
+<div id="groot"></div>
 
 
+<script src="js/monscriptquitue.js" defer></script>
 </body>
 </html>
