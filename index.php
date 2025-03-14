5e00a6a65d2b4b05bcbcccc60b959010b32248e9
Clément Veurrier <clement.veurrier@gmail.com>
2025-03-14 15:50:07 +0100

    index amelioré

--- index.php
+++ index.php
@@ -8,8 +8,50 @@
     <link rel="stylesheet" href="css/site.css">
 </head>
 <body>
-    
-<div id="groot"></div>
+  
+<h1> TOP ROBLOX GAMES </h1>
+
+<div id= wrapper>
+
+
+    <div id= main-nav>
+
+        <ul id= main-menu>
+
+            <li id= content-menu >Home</li>
+            <li id= content-menu >Trendings</li>
+            <li id= content-menu >Categories</li>
+            <li id= content-menu >Underrated</li>
+            <li id= content-menu >About us</li>
+                            
+        </ul>
+
+    </div>
+
+
+    <div id = main-container>
+
+    <h3>titre jeu en tendance</h3>
+    <p>image insere</p>
+
+    <p>description vite fait </p>
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
 
 
 <script src="js/monscriptquitue.js" defer></script>
