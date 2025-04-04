8d44f6cfc59ee5dbd73455557ff380f9f8212108
Clément Veurrier <clement.veurrier@gmail.com>
2025-04-04 19:41:23 +0200

    nouvelle version

--- include/connexion.php
+++ include/connexion.php
@@ -1,16 +1,16 @@
 <?php
     require('./connect.php');
 	
-	$CONNEXION = mysqli_connect (SERVEUR_BD, LOGIN_BD, PASS_BD);
+	$CONNEXION = mysqli_connect ("mariadb",MARIADB_USER,MARIADB_PASSWORD,MARIADB_DATABASE);
 	//Connexion au serveur de bases de données
 	if (mysqli_connect_errno()) {
-		echo 'Désolé, connexion au serveur ' . SERVEUR_BD . ' impossible, '. mysqli_connect_error(), "\n";
+		echo 'Désolé, connexion au serveur ' . MARIADB_DATABASE . ' impossible, '. mysqli_connect_error(), "\n";
     	exit();
 	}
 	// Sélection de la base de données
-	mysqli_select_db($CONNEXION, NOM_BD);
+	mysqli_select_db($CONNEXION, MARIADB_DATABASE);
 	if (mysqli_connect_errno()) {
-		echo 'Désolé, accès à la base ' . NOM_BD . ' impossible, '. mysqli_connect_error(), "\n";
+		echo 'Désolé, accès à la base ' . MARIADB_DATABASE . ' impossible, '. mysqli_connect_error(), "\n";
     	exit();
 	}
 	// Spécification de l'encodage UTF-8 pour dialoguer avec la BD
--- include/fonctions.php
+++ include/fonctions.php
@@ -2,8 +2,22 @@
 
 require('../connect.php');
 
-$CONNEXION = mysqli_connect(SERVEUR_BD, LOGIN_BD, PASS_BD, NOM_BD);
-
+$CONNEXION = mysqli_connect ("mariadb",MARIADB_USER,MARIADB_PASSWORD,MARIADB_DATABASE);
+//Connexion au serveur de bases de données
+if (mysqli_connect_errno()) {
+    echo 'Désolé, connexion au serveur ' . MARIADB_DATABASE . ' impossible, '. mysqli_connect_error(), "\n";
+    exit();
+}
+// Sélection de la base de données
+mysqli_select_db($CONNEXION, MARIADB_DATABASE);
+if (mysqli_connect_errno()) {
+    echo 'Désolé, accès à la base ' . MARIADB_DATABASE . ' impossible, '. mysqli_connect_error(), "\n";
+    exit();
+}
+// Spécification de l'encodage UTF-8 pour dialoguer avec la BD
+if (!mysqli_set_charset($CONNEXION, 'UTF8')) {
+    echo 'Erreur au chargement de l\'encodage UTF-8 : ', mysqli_connect_error(), "\n";
+}
 
 $sql = "SELECT id, nom, annee_sortie, nb_joueurs, description FROM jeux";
 $result = mysqli_query($CONNEXION, $sql);
